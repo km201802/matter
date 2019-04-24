ea42abb52c713af78f6482220628fa96b510c9e5
km201802 <36888473+km201802@users.noreply.github.com>
2019-04-24 14:06:19 -0400

    Create objetos.js

---
+++ js/objetos.js
@@ -0,0 +1,60 @@
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+var engine;
+var suelos = [];
+var circulos = [];
+
+function Circle(x, y, r){
+    var circle = Bodies.circle(x, y, r);
+    World.add(engine.world, circle);
+    
+    this.position = circle.position;
+    
+    this.draw = function(){
+        push();
+        translate(this.position.x, this.position.y);
+        ellipse(0, 0, r*2);
+        pop();
+    }
+}
+function Ground(x, y, w, h){
+    var options = {
+    
+    }
+    var ground = Bodies.rectangle(x, y, w, h);
+    World.add(engine.world, ground);
+    
+    this.position = ground.position;
+    
+    this.draw = function(){
+        push();
+        translate(this.position.x, this.position.y);
+        rectMode(CENTER);
+        rect(0, 0, w, h);
+        pop();
+    }
+}
+
+
+function setup(){
+    createCanvas(600,600);
+    engine = Engine.create();
+    circulos.push(new Circle(300,50,20));
+    suelos.push(new Ground(300,550,30,600))
+}
+function draw(){
+    background(100);
+    
+    for(var i=0; i<suelos.length; i++){
+        suelos[i].draw();
+    }
+    for(var i=0; i<circulos.length; i++){
+        circulos[i].draw();
+    }
+    
+    Engine.update(engine);
+}
