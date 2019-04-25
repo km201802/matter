9c339dd5ebed7087451387c32ae991c91d85212d
km201802 <36888473+km201802@users.noreply.github.com>
2019-04-25 07:31:16 -0400

    Update objetos.js

--- js/objetos.js
+++ js/objetos.js
@@ -8,46 +8,6 @@ var engine;
 var suelos = [];
 var circulos = [];
 
-function Circle(x, y, r){
-    var options = {
-        friction: 0,
-        restitution: 1,
-        gravityScale: 0,
-        
-    };
-    var circle = Bodies.circle(x, y, r, options);
-    World.add(engine.world, circle);
-    circle.velocity.x = 3;
-    circle.velocity.y = 5;
-    
-    this.position = circle.position;
-    
-    this.draw = function(){
-        push();
-        translate(this.position.x, this.position.y);
-        ellipse(0, 0, r*2);
-        pop();
-    }
-}
-function Ground(x, y, w, h){
-    var options = {
-        isStatic: true,
-        friction: 0,
-        restitution: 1
-    }
-    var ground = Bodies.rectangle(x, y, w, h, options);
-    World.add(engine.world, ground);
-    
-    this.position = ground.position;
-    
-    this.draw = function(){
-        push();
-        translate(this.position.x, this.position.y);
-        rectMode(CENTER);
-        rect(0, 0, w, h);
-        pop();
-    }
-}
 
 
 function setup(){
