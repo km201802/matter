3cd59a5f31668db42e197b2b99dbff1ac6b5f995
km201802 <36888473+km201802@users.noreply.github.com>
2019-04-25 11:25:54 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,26 +7,34 @@ var Engine = Matter.Engine,
 var engine;
 var suelos = [];
 var circulos = [];
-
+var width = innerWidth;
+var height = innerHeight;
 
 
 function setup(){
-    createCanvas(600,600);
+    width = innerWidth;
+    height = innerHeight;
+    createCanvas(width,height);
     engine = Engine.create();
     circulos.push(new Circle(300,50,20,false));
-    suelos.push(new Box(300,550,600,30,true))
+    suelos.push(new Box(width/2,height*0.95,width,30,true))
 }
+console.log(`${height} X ${width}`);
 function draw(){
     background(100);
-    /*if(mouseIsPressed()){
+    if(mouseIsPressed){
         circulos.push(new Circle(mouseX,mouseY,20,false));
-    }*/
+    }
     for(var i=0; i<suelos.length; i++){
         suelos[i].draw();
+        
     }
     for(var i=0; i<circulos.length; i++){
         circulos[i].draw();
+        if(circulos[i].borrar){
+            circulos.splice(i, 1);
+        }
     }
-    
+    console.log(circulos.length);
     Engine.update(engine);
 }
