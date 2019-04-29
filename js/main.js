f98e8d07fcd7625836f5442e5dc61dea3903c2dd
km201802 <36888473+km201802@users.noreply.github.com>
2019-04-29 11:42:39 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@
 var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
-    Bodies = Matter.Bodies;
+    Bodies = Matter.Bodies,
+    Constraint = Matter.Constraint;
 
 var engine;
 var suelos = [];
 var circulos = [];
 var cajas = [];
+var ropes = [];
 var width = innerWidth;
 var height = innerHeight;
 var world;
@@ -23,26 +25,41 @@ function setup(){
 
     world = engine.world;
     //circulos.push(new Circle(300,50,20,false));
-    suelos.push(new Box(width/2,height*0.95,width,30,PI/10,true));
-    //console.log(engine.world);
+    suelos.push(new Box(width/2,height*0.95,width,30,0,true));
+    
+    circulos.push(new Circle(300,50,20,true));
+    circulos.push(new Circle(400,50,20,false));
+    var constraint = Constraint.create({
+        bodyA: circulos[0].circle,
+        bodyB: circulos[1].circle,
+        length:500,
+    });
+    World.add(world, constraint);
 }
+var pos = 0
+function mouseWheel(event) {
+  pos += event.delta;
+}
+
+
 console.log(`${height} X ${width}`);
 function draw(){
     background(100);
     if(mouseIsPressed){
         //circulos.push(new Circle(mouseX,mouseY,20,false));
-        try{
-            cajas.push(new Box(mouseX, mouseY ,random(30, 60), radom(30, 60),0,false));
-        }cath(err){
-            alert(err);
-        }
-        //cajas.push(new Box(mouseX, mouseY ,random(30, 60), radom(30, 60),0,false));
+            cajas.push(new Box(mouseX, mouseY ,random(30, 60), random(30, 60),0,false));
     }
     for(var i=0; i<suelos.length; i++){
         suelos[i].draw();
     }
     for(var i=0; i<cajas.length; i++){
         cajas[i].draw();
+        if(cajas[i].borrar()){
+            cajas[i].a = pos/36;
+            cajas[i].eliminar();
+            cajas.splice(i, 1);
+            i--;
+        }
     }
     for(var i=0; i<circulos.length; i++){
         circulos[i].draw();
