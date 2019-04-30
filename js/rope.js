831891aca5dda9285e57fb067a0c6ac29e1059e8
km201802 <36888473+km201802@users.noreply.github.com>
2019-04-30 11:53:12 -0400

    Update rope.js

--- js/rope.js
+++ js/rope.js
@@ -1,6 +1,21 @@
-var circle = [];
+
+var circulos = [];
+var Circulos = [];
 var Rope = function(x, y, r, l, n){
-  for(var i=0; i<n; i++){
-  
-  }
+    this.static = true;
+    for(var i=0; i<n; i++){
+      Circulos.push(new Circle(x+(i*l),y,r, this.static));
+        this.static = false;
+    }
+    
+    for(var i=0; i<Circulos.length-1; i++){
+        //if(i!=Circulos.length){
+            var constraint = Constraint.create({
+                bodyA: Circulos[i].circle,
+                bodyB: Circulos[i+1].circle,
+                length:l,
+            });
+            World.add(world, constraint);
+        //}
+    }
 }
