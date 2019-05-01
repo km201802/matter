0d2be87f7242b3e3a53ade0f159696482737a042
km201802 <36888473+km201802@users.noreply.github.com>
2019-05-01 09:09:43 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // module aliases
+try{
 var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
@@ -82,4 +83,8 @@ function draw(){
     text( `${circulos.length} : ${world.bodies.length}` , 300, 40);
     pop();
     Engine.update(engine);
+}
+}
+catch(error){
+    alert(error);
 }
